584e91a12bd80c14cb149f9637b8ff7e2d11ce2e
Willem Wollebrants <willem@studiow.be>
2016-09-11 20:29:42 +0200

    Import

---
+++ src/EmitterAbstract.php
@@ -0,0 +1,91 @@
+<?php
+
+
+namespace Lune\Http\Emitter;
+
+use Psr\Http\Message\ResponseInterface;
+
+abstract class EmitterAbstract
+{
+
+    /**
+     * Helper function: converts header names to wordcase
+     * @param string $name
+     * @return string
+     */
+    private function toWordCase(string $name):string
+    {
+        $uc = ucwords(str_replace('-', ' ', $name));
+        return str_replace(' ', '-', $uc);
+    }
+
+    /**
+     * Makes sure the response has it's content length set
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    protected function withContentLengthHeader(ResponseInterface $response):ResponseInterface
+    {
+        $size = $response->getBody()->getSize();
+        if (!is_null($size)) {
+            $response = $response->withHeader('Content-Length', (string)$size);
+        }
+        return $response;
+    }
+
+
+    /**
+     * Sends the HTTP status line
+     *
+     * @param ResponseInterface $response
+     */
+
+    protected function sendStatus(ResponseInterface $response)
+    {
+        $status = vsprintf(
+            'HTTP/%s %d%s', [
+            $response->getProtocolVersion(),
+            $response->getStatusCode(),
+            rtrim(' ' . $response->getReasonPhrase())
+        ]);
+
+        header($status);
+    }
+
+    /**
+     * Loops through all headers and sents them
+     *
+     * @param ResponseInterface $response
+     */
+    protected function sendHttpHeaders(ResponseInterface $response)
+    {
+        $headers = $response->getHeaders();
+
+        array_map(function ($name) use ($headers) {
+            $this->sendHttpHeader(
+                $name,
+                $headers[$name]
+            );
+        }, array_keys($headers));
+    }
+
+    /**
+     * Sends a single HTTP header
+     *
+     * @param string $name
+     * @param array $values
+     */
+
+    private function sendHttpHeader(string $name, array $values)
+    {
+        array_map(function ($index) use ($name, $values) {
+            $header = vsprintf(
+                '%s: %s', [
+                $this->toWordCase($name),
+                $values[$index]
+            ]);
+            $replace = $index === 0;
+            header($header, $replace);
+        }, array_keys($values));
+    }
+}
---
+++ src/ResponseEmitter.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Lune\Http\Emitter;
+
+use Psr\Http\Message\ResponseInterface;
+use HttpRuntimeException;
+
+class ResponseEmitter extends EmitterAbstract
+{
+    public function emit(ResponseInterface $response)
+    {
+        if (headers_sent()) {
+            //Headers are already sent
+            throw new HttpRuntimeException("Cannot emit: headers already sent.");
+        }
+
+        //Make sure the response has it's content length set
+        if (!$response->hasHeader('Content-Length')) {
+            //add the Content-Length if necessary
+            $response = $this->withContentLengthHeader($response);
+        }
+
+        //Start emitting
+        //Emit the HTTP status line
+        $this->sendStatus($response);
+
+        //Emit the HTTP headers
+        $this->sendHttpHeaders($response);
+
+        //Emit the body
+        $this->sendBody($response);
+    }
+
+    /**
+     * Sends the body of the response
+     *
+     * @param ResponseInterface $response
+     */
+    private function sendBody(ResponseInterface $response)
+    {
+        echo $response->getBody();
+    }
+}
